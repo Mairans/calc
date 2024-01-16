ab347f51c8dd9c965a4d7c6035dc802740e87e8b
Mairans <142796352+Mairans@users.noreply.github.com>
2024-01-16 23:33:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -41,3 +41,40 @@ buttons.map((btn) => {
         }
     });
 });
+
+buttons.map((btn) => {
+    btn.addEventListener('touchend ', (e) => {
+        switch (e.target.innerText) {
+
+            default:
+                if (result.innerText === '0' && e.target.innerText !== '.') {
+                    result.innerText = e.target.innerText;
+                } else {
+                    result.innerText += e.target.innerText;
+                }
+                break;
+
+            case "AC":
+                result.innerText = '0'
+                break;
+                
+            case "+/-":
+                result.innerText = '-';
+                break;
+
+            case "%":
+                let passedText = result.innerText + '/100';
+                result.innerText = eval(passedText)
+                break;
+
+            case '=':
+                try {
+                    result.innerText = eval(result.innerText);
+                } catch (e) {
+                    result.innerText = 'Error!'
+                }
+                break;
+
+        }
+    });
+});
